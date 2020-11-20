3c216aa81256695b4cfc0d37cb440cbf31203fb0
Daniil <daniilbond1@gmail.com>
2020-11-20 11:44:52 +0300

    1

---
+++ Sum/Sum/Sum.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+/*
+ * Powering number.
+ *
+ * @param tests number of sums
+ * @param a left term
+ * @param b right term
+ * @return (a+b)
+ */
+
+void sum() {
+    int tests = 0;
+    int a = 0;
+    int b = 0;
+    int answer = 0;
+    cin >> tests;
+    for (int g = 0;g < tests;g++)
+    {
+        cout << "insert a: ";
+        cin >> a;
+        cout << "insert b: ";
+        cin >> b;
+        answer = a + b;
+        cout <<"answer: "<< answer << endl;
+    }
+}
+
+int main()
+{
+    sum();
+    return 0;
+}
---
+++ quadratic solution/quadratic solution/quadratic solution.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+/*
+ * Quadratic solution
+ *
+ * @param a first quadratic equation
+ * @param b second quadratic equation
+ * @param c third quadratic equation
+ * @param D discriminant
+ * @param x1 answer 
+ * @param x2 answer
+ * @return x,y
+ */
+
+void quadratic_solution() {
+    int a, b, c, D;
+    double x1,x2;
+
+
+    cin >> a >> b >> c;
+
+    D = b * b - 4 * a * c;
+
+    if (D >= 0) {
+        x1 = (-b - sqrt(D)) / (2 * a);
+        x2 = (-b + sqrt(D)) / (2 * a);
+        cout << x1 << endl;
+        cout << x2 << endl;
+    }
+    if (D < 0) {
+        cout << "No real roots\n";
+    }
+}
+
+
+int main()
+{
+    quadratic_solution();
+    return 0;
+}
